78cd3aac95bd5826aece9a0831c8de6bbef185d7
Jason Closterman <devjclosterman@gmail.com>
2023-05-12 22:02:12 -0700

    SQLI connected with no errors, Routes with minor errors but will fix next commit

--- login.php
+++ login.php
@@ -2,10 +2,10 @@
 <?php
  include_once 'header.php';
  ?>
- 
+
 <section class=signup-form>
    <h2>Log In</h2>
-   <form action="login.inc.php" method="post">
+   <form action="includes/login.inc.php" method="post">
     <input type="text" name="name" placeholder="Username/Email..">
     <input type="password" name="pwd" placeholder="Password..">
     <button type="Submit" name="submit">Login</button>
