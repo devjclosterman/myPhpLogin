f4825d2812c48d69c0b59080ef2de061781215c0
Jason Closterman <devjclosterman@gmail.com>
2023-05-12 19:07:27 -0700

    functionality tested on mysqli database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,3 +5,7 @@ $dBPassword = "";
 $dbName = "phpproject01";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dbName);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
