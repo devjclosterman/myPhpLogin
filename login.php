72f156feceacd3be33d396d4500ca11e6f6a9652
Jason Closterman <devjclosterman@gmail.com>
2023-05-13 09:51:24 -0700

    debugged error on name attribute with html forms

--- login.php
+++ login.php
@@ -6,10 +6,20 @@
 <section class=signup-form>
    <h2>Log In</h2>
    <form action="includes/login.inc.php" method="post">
-    <input type="text" name="name" placeholder="Username/Email..">
+    <input type="text" name="uid" placeholder="Username/Email..">
     <input type="password" name="pwd" placeholder="Password..">
     <button type="Submit" name="submit">Login</button>
   </form>
+  <?php
+  if(isset($_GET["error"])) {
+      if ($_GET["error"] == "emptyinput") {
+          echo "<p>Fill in all fields!</p>";
+      }
+      else if ($_GET["error"] == "wrongLogin") {
+          echo "<p>Incorrect Login Information!</p>";
+      } 
+  }
+?>
  </section>
  
  <?php
